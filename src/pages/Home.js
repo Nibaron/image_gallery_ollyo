4572fdd43ad515f812a6b20f2c79af94d69983cb
Nibaron Kumar Das <36547410+Nibaron@users.noreply.github.com>
2023-11-06 03:59:28 +0600

    image updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,33 +1,21 @@
 import React, { useState } from "react";
 import { CustomCard, Header } from "../components";
 
-import img1 from "../assets/images/card/image-1.webp";
-import img2 from "../assets/images/card/image-2.webp";
-import img3 from "../assets/images/card/image-3.webp";
-import img4 from "../assets/images/card/image-4.webp";
-import img5 from "../assets/images/card/image-5.webp";
-import img6 from "../assets/images/card/image-6.webp";
-import img7 from "../assets/images/card/image-7.webp";
-import img8 from "../assets/images/card/image-8.webp";
-import img9 from "../assets/images/card/image-9.webp";
-import img10 from "../assets/images/card/image-10.jpeg";
-import img11 from "../assets/images/card/image-11.jpeg";
-import img12 from "../assets/images/card/upload.png";
 
 export const Home = () => {
   const [cardData, setCardData] = useState([
-    { id: 1, image: img1, clicked: false },
-    { id: 2, image: img2, clicked: false },
-    { id: 3, image: img3, clicked: false },
-    { id: 4, image: img4, clicked: false },
-    { id: 5, image: img5, clicked: false },
-    { id: 6, image: img6, clicked: false },
-    { id: 7, image: img7, clicked: false },
-    { id: 8, image: img8, clicked: false },
-    { id: 9, image: img9, clicked: false },
-    { id: 10, image: img10, clicked: false },
-    { id: 11, image: img11, clicked: false },
-    { id: 12, image: img12, clicked: false },
+    { id: 1, image: "images/image-1.webp", clicked: false },
+    { id: 2, image: "images/image-2.webp", clicked: false },
+    { id: 3, image: "images/image-3.webp", clicked: false },
+    { id: 4, image: "images/image-4.webp", clicked: false },
+    { id: 5, image: "images/image-5.webp", clicked: false },
+    { id: 6, image: "images/image-6.webp", clicked: false },
+    { id: 7, image: "images/image-7.webp", clicked: false },
+    { id: 8, image: "images/image-8.webp", clicked: false },
+    { id: 9, image: "images/image-9.webp", clicked: false },
+    { id: 10, image: "images/image-10.jpeg", clicked: false },
+    { id: 11, image: "images/image-11.jpeg", clicked: false },
+    { id: 12, image: "images/upload.png", clicked: false },
   ]);
 
   const [selectedCard, setSelectedCard] = useState([]);
--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -5,8 +5,8 @@ export const AllRoutes = () => {
   return (
     <div className="dark:bg-slate-800">
         <Routes>
-              <Route path="/" element={ <Home title="Movie MagNet"/> } />
-              <Route path="*" element={ <PageNotFound title="Page Not Found"/> } />
+              <Route path="/" element={ <Home/> } />
+              <Route path="*" element={ <PageNotFound/> } />
         </Routes>
     </div>
   )
